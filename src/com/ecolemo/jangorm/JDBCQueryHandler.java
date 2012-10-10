0186c49effda1a92d0524dd1f41543eca3f6b461
Youngrok pak <pak.youngrok@gmail.com>
2012-10-11 02:24:27 +0900

    mysql support

--- src/com/ecolemo/jangorm/JDBCQueryHandler.java
+++ src/com/ecolemo/jangorm/JDBCQueryHandler.java
@@ -1,19 +1,20 @@
 package com.ecolemo.jangorm;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import com.ecolemo.jangorm.manager.ModelManager;
 
 public abstract class JDBCQueryHandler<T> {
 
 	public abstract T run(PreparedStatement stmt, String query, Object... parameters) throws SQLException;
 
-	public T execute(String connUrl, String query, Object... parameters) {
+	public T execute(ModelManager manager, String query, Object... parameters) {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
-			conn = DriverManager.getConnection(connUrl);
+			conn = manager.getConnection();
 			stmt = conn.prepareStatement(query);
 			for (int i = 0; i < parameters.length; i++) {
 				stmt.setObject(i + 1, parameters[i]);
---
+++ src/com/ecolemo/jangorm/manager/MySQLModelManager.java
@@ -0,0 +1,41 @@
+package com.ecolemo.jangorm.manager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.ecolemo.jangorm.ModelException;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+public class MySQLModelManager extends JDBCModelManager {
+
+	private final String user;
+	private final String password;
+
+	public MySQLModelManager(String host, String database, String user, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		url = String.format("jdbc:mysql://%s/%s", host, database);
+		this.user = user;
+		this.password = password;
+	}
+	
+	@Override
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+	
+	@Override
+	public ConnectionSource getConnectionSource() {
+		try {
+			return new JdbcConnectionSource(url, user, password);
+		} catch (SQLException e) {
+			throw new ModelException(e);
+		}
+	}
+	
+}
