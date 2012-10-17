cc8839cb5cdc9dd5515a5c2d59e55270ffe2fc7a
Youngrok pak <pak.youngrok@gmail.com>
2012-10-17 22:28:31 +0900

    date formats

--- src/com/ecolemo/jangorm/util/DateFormats.java
+++ src/com/ecolemo/jangorm/util/DateFormats.java
@@ -1,8 +1,10 @@
 package com.ecolemo.jangorm.util;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class DateFormats {
 	public static SimpleDateFormat plainDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
+	public static SimpleDateFormat dateOnly = new SimpleDateFormat("yyyyMMdd");
+	public static DateFormat humaneDate = DateFormat.getDateInstance(DateFormat.MEDIUM);
 }
